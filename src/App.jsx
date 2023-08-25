de83429ad8d796bc2333c7e21ef818796308c264
Gian De las Llagas <crimpsonplays@gmail.com>
2023-08-26 04:43:39 +0800

    imported browser routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+//Routes
+import Dashboard from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />
+  },
+  {
+    path: "/contact",
+    element: <h1>Contact</h1>
+  },
+]);
+
 function App() {
   return (
     <div className="App">
-    
+      <RouterProvider router={router} />
     </div>
   )
 }
