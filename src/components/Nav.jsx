4ae6242fe0533f9ea1836e87307895edf6971323
Gian De las Llagas <crimpsonplays@gmail.com>
2023-08-26 05:46:59 +0800

    add Navigator

---
+++ src/components/Nav.jsx
@@ -0,0 +1,42 @@
+// react-router-dom imports
+import { Form, NavLink } from "react-router-dom"
+
+//assets
+import logomark from "../assets/logomark.svg"
+
+//library
+import { TrashIcon } from '@heroicons/react/24/solid'
+
+const Nav = ({ userName }) => {
+  return (
+    <nav>
+        <NavLink
+            to="/"
+            aria-label="Go to Home"
+        >
+        <img src={logomark} alt="" height={30}/>
+        <span>Budget</span>
+        </NavLink>
+        {
+            //userName && (
+                <Form
+                    method="post"
+                    action="/logout"
+                    onSubmit={(event) => {
+                        if(!confirm("Are you sure you want to delete all your data?")) {
+                            event.preventDefault()
+                        }
+                    }} 
+                >
+                    <button type="submit" className="btn btn--warning">
+                        <span>Delete User</span>
+                        <TrashIcon width={20} />
+                    </button>
+                </Form>
+            //)
+        }
+    </nav>
+  )
+}
+
+export default Nav
