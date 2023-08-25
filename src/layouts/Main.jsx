052c375a81e2b8f5eb8d38d0b7b3700a2b2b1796
Gian De las Llagas <crimpsonplays@gmail.com>
2023-08-26 05:47:15 +0800

    added main class

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,33 @@
+// react-router-dom imports
+import { Outlet, useLoaderData } from "react-router-dom";
+
+// helper functions
+import { fetchData } from "../helpers"
+
+//assets
+import wave from "../assets/wave.svg";
+
+//components
+import Nav from "../components/Nav";
+
+// loader
+export function mainLoader(){
+    const userName = fetchData("userName");
+    return { userName }
+}
+
+const Main = () => {
+    const { userName } = useLoaderData()
+
+    return (
+        <div className="layout">
+            <Nav userName={userName} /> 
+            <h1>Main</h1>
+            <main>
+                <Outlet />
+            </main>
+            <img src={wave} alt="" />
+        </div>
+    )
+}  
+export default Main
