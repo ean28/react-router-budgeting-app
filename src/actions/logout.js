b3f131a704d7d291968a4a52438d97b06f57b439
Gian De las Llagas <crimpsonplays@gmail.com>
2023-08-28 04:42:25 +0800

    logout config

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,8 +1,13 @@
 import { redirect } from "react-router-dom";
+import { deleteItem } from "../helpers";
+import { toast } from "react-toastify";
 
 export async function logoutAction(){
     //delete the user
-
-    //redirect
+    deleteItem({
+        key: "userName"
+    })
+    toast.success("Success")
+    //redirect them to main page
     return redirect("/")
 }
